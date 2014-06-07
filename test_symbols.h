01cbbcb4f769062b2bdb958f9cff039c82fc1e7a
joscox <jcox@nmt.edu>
2014-06-07 00:07:31 -0600

    Update and rename symbols.h to test_symbols.h

---
+++ test_symbols.h
@@ -0,0 +1,12 @@
+#ifndef SYMBOLS_H
+#define SYMBOLS_H
+
+#include <math.h>
+
+enum symbols{
+	BAD, NUM, COND, VAR, OP, ELSE, EOL
+};
+char *symbolnames[] = {"BAD", "NUM", "COND", "VAR", "OP", "ELSE", "EOL"};
+
+ 
+#endif //SYMBOLS_H
